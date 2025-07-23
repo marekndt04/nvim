c654d8fa1a224a2e8e35515026485d0a7f3f856d
Marek <m.arendt04@gmail.com>
2025-07-23 19:55:05 +0200

    dap configuration added

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -68,6 +68,47 @@ return {
             require("configs.lint")
         end,
     },
+
+    {
+        "mfussenegger/nvim-dap",
+        config = function()
+            require("configs.dap")
+        end,
+    },
+
+    {
+        "nvim-neotest/nvim-nio",
+    },
+
+    {
+        "rcarriga/nvim-dap-ui",
+        dependencies = {
+            "mfussenegger/nvim-dap",
+            "nvim-neotest/nvim-nio",
+        },
+        config = function()
+            require("configs.dap-ui")
+        end,
+    },
+
+    {
+        "mfussenegger/nvim-dap-python",
+        ft = "python",
+        dependencies = {
+            "mfussenegger/nvim-dap",
+            "rcarriga/nvim-dap-ui",
+        },
+        config = function()
+            require("configs.dap-python")
+        end,
+    },
+    {
+        "jay-babu/mason-nvim-dap.nvim",
+        event = "VeryLazy",
+        config = function()
+            require("configs.mason-dap")
+        end,
+    },
     -- test new blink
     -- { import = "nvchad.blink.lazyspec" },
 
